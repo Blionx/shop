8379373a5aaef8dca82c01228973080a2eeb8f56
Luis Pens <blion_pens@hotmail.com>
2022-11-01 12:45:47 -0300

    creando estructura principal del sitio

---
+++ back/mainback/src/main/java/civsys/shop/mainback/MainbackApplication.java
@@ -0,0 +1,13 @@
+package civsys.shop.mainback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainbackApplication.class, args);
+	}
+
+}
---
+++ back/mainback/src/test/java/civsys/shop/mainback/MainbackApplicationTests.java
@@ -0,0 +1,13 @@
+package civsys.shop.mainback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
